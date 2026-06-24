24b111fe7c42c446063c3748226840250ecda763
joncastillo <jon.castillo.dev@gmail.com>
2026-06-25 01:14:02 +1000

    feat: add more fonts to rich text editor

--- src/components/RichTextEditor.tsx
+++ src/components/RichTextEditor.tsx
@@ -7,6 +7,11 @@ const icons = ReactQuill.Quill.import('ui/icons')
 icons['undo'] = '<svg viewbox="0 0 18 18"><polygon class="ql-fill ql-stroke" points="6 10 4 12 2 10 6 10"></polygon><path class="ql-stroke" d="M8.09,13.91A4.6,4.6,0,0,0,9,14,5,5,0,1,0,4,9"></path></svg>'
 icons['redo'] = '<svg viewbox="0 0 18 18"><polygon class="ql-fill ql-stroke" points="12 10 14 12 16 10 12 10"></polygon><path class="ql-stroke" d="M9.91,13.91A4.6,4.6,0,0,1,9,14a5,5,0,1,1,5-5"></path></svg>'
 
+// Register custom fonts
+const Font = ReactQuill.Quill.import('formats/font')
+Font.whitelist = ['inter', 'merriweather', 'comic-sans']
+ReactQuill.Quill.register(Font, true)
+
 interface Props {
   value: string
   onChange: (val: string) => void
@@ -32,7 +37,7 @@ export default function RichTextEditor({ value, onChange, placeholder, className
   const modules = useMemo(() => ({
     toolbar: {
       container: [
-        [{ 'header': [1, 2, 3, 4, 5, 6, false] }, { 'font': [] }, { 'size': [] }],
+        [{ 'header': [1, 2, 3, 4, 5, 6, false] }, { 'font': [false, 'inter', 'merriweather', 'comic-sans'] }, { 'size': [] }],
         ['bold', 'italic', 'underline', 'strike'],
         [{ 'color': [] }, { 'background': [] }],
         [{ 'list': 'ordered' }, { 'list': 'bullet' }],
